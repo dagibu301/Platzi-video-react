052da1e84d4a42ea0b021364d91f53b164d2c0e4
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-01 21:20:22 -0400

    scss webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  devServer: {
+    open: true,
+    hot: true,
+  },
+  mode: "development",
   resolve: {
     extensions: [".js", ".jsx"],
   },
@@ -14,24 +21,32 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: "babel-loader",
       },
       {
         test: /\.html$/,
+        use: "html-loader",
+      },
+      {
+        test: /\.(s*)css$/,
         use: [
           {
-            loader: "html-loader",
+            loader: MiniCssExtractPlugin.loader,
           },
+          "css-loader",
+          "sass-loader",
         ],
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: "public/index.html",
+      filename: "index.html",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "assets/[name].css",
     }),
   ],
 };
