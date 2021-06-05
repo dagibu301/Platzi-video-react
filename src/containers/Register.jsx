14c8c2e4075f11be7fe5e286575fb499a002bc56
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-05 18:32:34 -0400

    register action and reducer

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { registerRequest } from '../actions/index';
 import '../assets/styles/components/Register.scss';
 
-const Register = () => (
-  <section className='register'>
-    <section className='register__container'>
-      <h2>Register</h2>
-      <form className='register__container--form'>
-        <input className='input' type='text' placeholder='Name' />
-        <input className='input' type='text' placeholder='Email' />
-        <input className='input' type='password' placeholder='Password' />
-        <button type='button' className='button'>Register</button>
-      </form>
-      <Link to='/login'>Login</Link>
-    </section>
-  </section>
-);
+const Register = (props) => {
+  const [form, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-export default Register;
+  const handleInput = (e) => {
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.registerRequest(form);
+    props.history.push('/');
+  };
+
+  return (
+    <section className='register'>
+      <section className='register__container'>
+        <h2>Register</h2>
+        <form className='register__container--form' onSubmit={handleSubmit}>
+          <input
+            className='input'
+            type='text'
+            placeholder='Name'
+            name='name'
+            onChange={handleInput}
+          />
+          <input
+            className='input'
+            type='text'
+            placeholder='Email'
+            name='email'
+            onChange={handleInput}
+          />
+          <input
+            className='input'
+            type='password'
+            placeholder='Password'
+            name='password'
+            onChange={handleInput}
+          />
+          <button type='submit' className='button'>Register</button>
+        </form>
+        <Link to='/login'>Login</Link>
+      </section>
+    </section>
+  );
+};
+
+const mapDispatchToProps = {
+  registerRequest,
+};
+
+export default connect(null, mapDispatchToProps)(Register);
