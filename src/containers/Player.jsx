964ed273de129528fa192052b4d2dca9b031b516
David Giraldo <davidgiraldobueno@gmail.com>
2021-06-06 19:15:59 -0400

    player

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+/* eslint-disable react/destructuring-assignment */
+import React, { useLayoutEffect } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { getVideoSource } from '../actions/index';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => {
-  return (
+const Player = (props) => {
+  const { id } = props.match.params;
+  const hasPlaying = Object.keys(props.playing).length > 0;
+
+  useLayoutEffect(() => {
+    props.getVideoSource(id);
+  }, []);
+
+  return hasPlaying ? (
     <div className='Player'>
       <video controls autoPlay>
-        <source src='' type='video/mp4' />
+        <source src={props.playing.source} type='video/mp4' />
       </video>
       <div className='Player-back'>
-        <button type='button'>Return</button>
+        <button type='button' onClick={() => props.history.goBack()}>Return</button>
       </div>
     </div>
-  );
+  ) :
+    (<Redirect to='/404/' />);
 };
 
-export default Player;
+const mapStateToProps = (state) => {
+  return {
+    playing: state.playing,
+  };
+};
+
+const mapDispatchToProps = {
+  getVideoSource,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
